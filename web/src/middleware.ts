917e4b017f0a4eeed493fe31bd16cd45a9388bd8
ryokosuge <ryo.kosuge@gmail.com>
2022-12-15 16:04:06 +0900

    feat:	認証画面へのredirect

--- web/src/middleware.ts
+++ web/src/middleware.ts
@@ -1,8 +1,45 @@
 import { NextRequest, NextResponse } from "next/server";
+import { v4 as uuid } from "uuid";
 
 export const middleware = (request: NextRequest) => {
-  console.info(request);
+  const url = request.nextUrl.clone();
+  const shop = url.searchParams.get("shop") ?? "";
+  const callbackURL = getCallbackURL();
+  if (url.pathname !== callbackURL.pathname) {
+    const state = generateState();
+    const authorizationURL = getAuthorizationURL(url, callbackURL, shop, state);
+    const res = NextResponse.redirect(authorizationURL);
+    res.cookies.set("state", state);
+    return res;
+  }
   const res = NextResponse.next();
-  console.info(res);
   return res;
+};
+
+const CALLBACK_URL_PATH = "/auth/callback";
+
+const getCallbackURL = () => {
+  return new URL(`${process.env.APP_URL}${CALLBACK_URL_PATH}`);
+};
+
+const generateState = () => {
+  return uuid();
+};
+
+const getAuthorizationURL = (
+  url: URL,
+  callbackURL: URL,
+  myshopifyDomain: string,
+  state: string,
+) => {
+  const params = new URLSearchParams(url.searchParams);
+  params.set("client_id", process.env.SHOPIFY_API_KEY);
+  params.set("scope", process.env.SCOPES);
+  params.set("redirect_uri", callbackURL.toString());
+  params.set("state", state);
+  const authorizationURL = new URL(
+    `https://${myshopifyDomain}/admin/oauth/authorize`,
+  );
+  authorizationURL.search = params.toString();
+  return authorizationURL;
 };
